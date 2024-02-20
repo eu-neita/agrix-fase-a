8202095c8337049bee88e89f8e115604974347f5
nathan <neitass.music@gmail.com>
2024-02-20 14:25:56 -0300

    feat: farm entity

---
+++ src/main/java/com/betrybe/agrix/models/entities/Farm.java
@@ -0,0 +1,44 @@
+package com.betrybe.agrix.models.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "farm")
+public class Farm {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String name;
+  private int size;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public void setSize(int size) {
+    this.size = size;
+  }
+
+
+}
