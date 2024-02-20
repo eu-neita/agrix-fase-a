87f7383da9cf6e712901b08af44406fd0880346c
nathan <neitass.music@gmail.com>
2024-02-20 14:58:40 -0300

    feat: endpoint /farms

--- src/main/java/com/betrybe/agrix/controllers/FarmController.java
+++ src/main/java/com/betrybe/agrix/controllers/FarmController.java
@@ -1,7 +1,14 @@
 package com.betrybe.agrix.controllers;
 
+import com.betrybe.agrix.controllers.dto.FarmDto;
+import com.betrybe.agrix.controllers.dto.ResponseDTO;
+import com.betrybe.agrix.models.entities.Farm;
 import com.betrybe.agrix.services.FarmService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class FarmController {
   private final FarmService farmService;
 
+  public FarmService getFarmService() {
+    return farmService;
+  }
+
   @Autowired
   public FarmController(FarmService farmService) {
     this.farmService = farmService;
   }
+
+  @PostMapping()
+  public ResponseEntity<ResponseDTO<Farm>> createBook(@RequestBody FarmDto farmDTO) {
+    Farm newFarm = farmService.insertFarm(farmDTO.toFarm());
+    ResponseDTO<Farm> responseDTO = new ResponseDTO<>("Livro criado com sucesso!", newFarm);
+    return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+  }
+
 }
